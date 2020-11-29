1b751bf571fb1a64ee0d22ef81a2ff668f0128f5
maciek <bma@post.pl>
2020-11-29 11:10:39 +0100

    init commit

---
+++ src/Components/List.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const List = () => {
+    let [readings, setReadings] = useState([])
+
+    useEffect(() => {
+        axios.get()
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default List
