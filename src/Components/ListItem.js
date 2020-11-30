40fa9e0df68d25bff4a03746553d280123bfcfb1
maciek <bma@post.pl>
2020-11-30 19:58:34 +0100

    styling

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -2,10 +2,15 @@ import React from 'react'
 import * as classes from './ListItem.module.css'
 
 const ListItem = (props) => {
+
+    let date = (new Date(props.element.readingTime)).toLocaleDateString()
+    let time = (new Date(props.element.readingTime)).toLocaleTimeString()
+
     return (
-        <div style={{marginTop: '5px'}}>
+        <div style={{marginTop: '10px'}}>
             <div style={{width: '30px'}} className={classes.item}>{props.index}</div>
-            <div style={{width: '200px'}} className={classes.item}>{props.element.readingTime}</div>
+            <div style={{width: '82px'}} className={classes.item}>{date}</div>
+            <div style={{width: '68px'}} className={classes.item}>{time}</div>
             <div style={{width: '40px'}} className={classes.item}>{props.element.readingValue}</div>
         </div>
     )
